0c837981d7c2aeb9f28c94fab260ebf5b2ae705b
axel alejandro morel <alejandro.morel1905@gmail.com>
2022-05-10 00:38:04 -0300

    Initial Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APlogoComponent } from './components/aplogo/aplogo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APlogoComponent,
+    SocialComponent,
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
